8cb50303dd2fc76c9e7f6077dfd0aef53e2afdd2
tharindu <tlakmal800@gmail.com>
2025-04-09 17:02:53 +0530

    connectMongodb

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(
+  () => {
+    console.log('MongoDB connected successfully');
+  }
+).catch((err) => {
+  console.error('MongoDB connection error:', err);
+});
+
 const app = express();
 app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
+  console.log('Server is running on port 3000');
 });
