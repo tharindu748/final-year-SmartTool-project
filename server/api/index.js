42b800afe2481b4fcd11a94417009df74caa3c4f
tharindu <tlakmal800@gmail.com>
2025-04-10 01:13:30 +0530

    create signup apirout

--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoute from './routes/user.route.js';
-
+import authRoute from './routes/auth.route.js';
+  
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(
@@ -14,9 +15,12 @@ mongoose.connect(process.env.MONGO).then(
 });
 
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
 
 
 app.use('/api/user', userRoute);
+app.use('/api/auth', authRoute); // This should be replaced with the actual auth route when implemented
